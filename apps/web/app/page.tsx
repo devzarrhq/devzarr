ccffc3572fc3c49ccbb588082c8d16aa4b9823e3
[dyad] <gaggleoffools@gmail.com>
2025-08-23 00:16:54 +0900

    [dyad] Created a minimal Tailwind test page to debug Tailwind processing - wrote 2 file(s) + extra files edited outside of Dyad

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,19 +1,5 @@
-import Sidebar from "./components/Sidebar";
-import Topbar from "./components/Topbar";
-import Feed from "./components/Feed";
+import TestTailwind from "./test-tailwind";
 
 export default function HomePage() {
-  return (
-    <div className="flex min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800">
-      <Sidebar />
-      <div className="flex-1 flex flex-col md:ml-64">
-        <Topbar />
-        <main className="flex-1 w-full">
-          {/* Test Tailwind */}
-          <div className="test-tailwind">TAILWIND TEST</div>
-          <Feed />
-        </main>
-      </div>
-    </div>
-  );
+  return <TestTailwind />;
 }
---
+++ apps/web/app/test-tailwind.tsx
@@ -0,0 +1,9 @@
+export default function TestTailwind() {
+  return (
+    <div className="p-10">
+      <div className="bg-accent-blue text-4xl text-white p-4 rounded">
+        If you see this blue box, Tailwind is working!
+      </div>
+    </div>
+  );
+}
