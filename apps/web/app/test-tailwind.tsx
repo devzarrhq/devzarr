b0e046ab66d8f44cf8c181ebaef5cca859a4d2c0
c0ri <gaggleoffools@gmail.com>
2025-08-23 00:38:49 +0900

    debug with test-tailwind.tsx

--- apps/web/app/test-tailwind.tsx
+++ apps/web/app/test-tailwind.tsx
@@ -1,9 +1,12 @@
 export default function TestTailwind() {
   return (
-    <div className="p-10">
-      <div className="bg-accent-blue text-4xl text-white p-4 rounded">
+    <div className="p-10 space-y-4">
+      <div className="bg-blue-500 text-4xl text-white p-4 rounded">
         If you see this blue box, Tailwind is working!
       </div>
+      <p className="text-red-500">
+        If you see this red text, Tailwind is compiling default utilities.
+      </p>
     </div>
   );
 }
