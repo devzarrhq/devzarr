9501407486a0d9a6512b16999f217335236d82d5
[dyad] <gaggleoffools@gmail.com>
2025-08-23 11:16:16 +0900

    [dyad] Fixed PostCSS config to use array format for Next.js - wrote 1 file(s) + extra files edited outside of Dyad

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: {
-    // Patch: alias for Next.js compatibility
-    'tailwindcss': require('@tailwindcss/postcss'),
-    autoprefixer: {},
-  },
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
 }
