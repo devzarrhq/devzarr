f080227ef568d6b3f568fe58c03d31b934a91630
[dyad] <gaggleoffools@gmail.com>
2025-08-23 12:20:17 +0900

    [dyad] Added name to web package and fixed Tailwind config for builds - wrote 2 file(s) + extra files edited outside of Dyad

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}"
-    // Do NOT include any parent or workspace directories here!
   ],
   theme: {
     extend: {},
