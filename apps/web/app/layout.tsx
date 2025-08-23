d747d46ffb4041fdf32e58988d7dc71cc5ba9483
[dyad] <gaggleoffools@gmail.com>
2025-08-23 11:02:23 +0900

    [dyad] Tested Tailwind by adding class to layout body - wrote 1 file(s) + extra files edited outside of Dyad

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from './theme-context'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen min-w-full transition-colors">
+      <body className="min-h-screen min-w-full transition-colors bg-blue-500">
         <ThemeProvider>
           {children}
         </ThemeProvider>
