80edd27b0b1bcf738c04d02e6e20686236dac273
c0ri <gaggleoffools@gmail.com>
2025-09-15 13:40:16 +0900

    bug fix with scrolling

--- apps/web/app/cliques/[id]/page.tsx
+++ apps/web/app/cliques/[id]/page.tsx
@@ -90,9 +90,9 @@ export default function CliquePage({ params }: { params: { id: string } }) {
 
   return (
     <CliqueMembersProvider cliqueId={clique.id} initial={members}>
-      <div className="flex min-h-screen w-full flex-row bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800">
+  <div className="flex h-screen w-full flex-row bg-gradient-to-br from-gray-950 via-gray-900 to-gray-800">
         <Sidebar />
-        <div className="flex flex-1 flex-col min-h-screen">
+  <div className="flex flex-1 flex-col h-screen">
           <Topbar />
           <div className="flex flex-1 flex-row min-h-0">
             <div className="flex-1 flex flex-col md:ml-64 lg:mr-[340px] px-4 min-h-0">
